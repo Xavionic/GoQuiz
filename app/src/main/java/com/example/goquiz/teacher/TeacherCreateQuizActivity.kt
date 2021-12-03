dd1926b777fbc0feb5338534abd27972b3500aed
Xavionic <dotasabili@gmail.com>
2021-12-04 02:21:21 +0700

    Integrating create quiz with firebase

--- app/src/main/java/com/example/goquiz/teacher/TeacherCreateQuizActivity.kt
+++ app/src/main/java/com/example/goquiz/teacher/TeacherCreateQuizActivity.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.goquiz.R
+import com.example.goquiz.data.TempQuestion
+import com.example.goquiz.student.StudentMainMenuActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.teacher_create_quiz.*
@@ -24,21 +26,51 @@ class TeacherCreateQuizActivity : AppCompatActivity(){
     }
 
     fun createQuiz(view: View){
-        val teacher_uid = auth.uid
-        val desc = editTextDescription.text.toString()
-        val start_time = editTextStartTime.text.toString()
-        val end_time = editTextEndTime.text.toString()
-        if (desc == "" || start_time == "" || end_time == ""){
+        val teacher_uid = auth.uid.toString()
+        val desc = editTextDescription.text
+        val start_time = editTextStartTime.text
+        val end_time = editTextEndTime.text
+
+        val quizData = mapOf<String, String>(
+            "description" to desc.toString(),
+            "end_time" to end_time.toString(),
+            "start_time" to start_time.toString(),
+            "teacher_uid" to teacher_uid
+        )
+
+
+        if (desc.toString() == "" || start_time.toString() == "" || end_time.toString() == ""){
             Toast.makeText(this, "Fill the create quiz form correctly!", Toast.LENGTH_SHORT).show()
         }else{
-            Toast.makeText(this, "Berhasil membuat Quiz!", Toast.LENGTH_LONG).show()
+
+            dbref = FirebaseDatabase.getInstance().getReference("/tmp_quizess")
+
+            dbref.addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    var buffer = ""
+
+                    for (quiz in snapshot.children){
+                        buffer = quiz.key.toString()
+                    }
+
+                    var quiz_id : Int = buffer.toInt() + 1
+
+                    val quiz = dbref.child(quiz_id.toString())
+                    quiz.setValue(quizData)
+
+                    Toast.makeText(applicationContext, "Berhasil membuat Quiz!", Toast.LENGTH_LONG).show()
+                    val intent= Intent(applicationContext, TeacherMainMenuActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(applicationContext, error.message, Toast.LENGTH_LONG).show()
+                }
+            })
 
 
 
-
-            val intent= Intent(this, TeacherMainMenuActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }
